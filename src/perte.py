1a5aebdf493474042879084c6bd7aa3eb3d3786d
pluszkis <pluszkis@pc5403h.lan.esiee.fr>
2018-11-15 16:13:03 +0100

    gitignore + download commentaire sur pertes.json

--- src/perte.py
+++ src/perte.py
@@ -3,7 +3,8 @@ from donnee import Donnee
 class Perte():
     DATA = list()
     _meta = Donnee(
-        url             = "https://data.sncf.com/explore/dataset/objets-trouves-gares/download/?format=json&timezone=Europe/Berlin",
+        url             = "https://data.sncf.com/explore/dataset/objets-trouves-gares/download/?format=json&timezone=Europe/Berlin", 
+        #si vous telechargez ce fichier manuellement veuillez le renommer en "pertes.json"
         file_name       = "pertes.json",
         keys            = ["date", "gc_obo_nom_recordtype_sc_c", "gc_obo_gare_origine_r_code_uic_c",], 
         key_aliases     = ["date", "type", "uic",], 

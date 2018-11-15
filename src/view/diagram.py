d422c02cf751506743dcc834270ece6e17d43af8
rivettie <rivettie@pc5403i.lan.esiee.fr>
2018-11-15 18:01:04 +0100

    diagram essai

--- src/view/diagram.py
+++ src/view/diagram.py
@@ -1,3 +1,18 @@
-class Diagram:
+import numpy as np
+import matplotlib.pyplot as plt
 
-    pass
+class Diagram:
+    #Afficher un histogramme representant le nombre de pertes par années. abscisse: année / ordonnée: nb perte
+    def __init__(self,nbLostObjPerYears):
+        self.nbLostObjPerYears = nbLostObjPerYears
+
+    def drawDiagram(self):
+        listeNb = list()
+        listeBins = set()
+        for year in self.nbLostObjPerYears.keys():
+            list.append(self.nbLostObjPerYears[year])
+            listeBins.add(year)
+        
+        plt.hist(listeNb, bin=listeBins)
+
+        #TODO APPEL DANS LE MAIN
